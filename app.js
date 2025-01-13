24f3fbee0ecc23f971ddc725d09f6fa16f06db13
tmaPeniel <taouemap@gmail.com>
2025-01-13 10:16:04 +0100

    Edit button navigation

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const filters = document.querySelectorAll('button:not(.add-task)');
-console.log(filters)
+console.log(filters);
 filters.forEach(button=>{
     button.addEventListener("click", ()=>{
-        filters.forEach(item=> item.classList.remove('active'))
+        filters.forEach(item=> item.classList.remove('active'));
         button.classList.add('active');
     })
 })
 
-const addTask = document.getElementById("add-task")
-console.log(addTask)
+const addTask = document.getElementById("add-task");
+console.log(addTask);
 addTask.addEventListener("click", ()=>{
     window.location.href= "form.html";
+})
+
+const editTasks = document.querySelectorAll('.edit');
+console.log(editTasks);
+editTasks.forEach(item=>{
+    item.addEventListener("click", ()=>{
+        window.location.href= "form.html";
+    })
 })
